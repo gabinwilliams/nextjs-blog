861cb047c63490fbca607ba8949312cc3ff3c2ce
gabinwilliams <scoutsnknives@gmail.com>
2023-02-13 11:53:36 +0100

    starting setup of nextjs blog

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const HomePage = ({}: Props) => {
+    return <h1>Hello from NextJS 13!!</h1>;
+};
+
+export default HomePage;
--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        reactStrictMode: true,
+        swcMinify: true,
+        appDir: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
